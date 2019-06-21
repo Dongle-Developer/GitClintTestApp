cc4a01dba95d72efeb6715c388b0f2ae0ccbbb91
dongle <dongle@stunitas.com>
2019-06-21 11:24:07 +0900

    - branch Test1

--- app/src/main/java/com/devleop/dongle/gitclinttestapp/ui/MainActivity.kt
+++ app/src/main/java/com/devleop/dongle/gitclinttestapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.devleop.dongle.gitclinttestapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.devleop.dongle.gitclinttestapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("11","11")
+
     }
 }
